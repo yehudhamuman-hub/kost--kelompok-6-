c5279d41ce6611acea29b2594ef2abae7ebd55a0
yehudhamuman <shinryujin4646@gmail.com>
2026-05-28 16:44:35 +0700

    feature/menambah class kamar penuh error

---
+++ exceptions/custom_exceptions.py
@@ -0,0 +1,14 @@
+class KamarPenuhError(Exception):
+    pass
+
+
+class PenghuniTidakDitemukanError(Exception):
+    pass
+
+
+class KamarTidakDitemukanError(Exception):
+    pass
+
+
+class PembayaranGandaError(Exception):
+    pass
