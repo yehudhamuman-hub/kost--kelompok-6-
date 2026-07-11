6afe3589f056eeff6001ace66860ebb697fa0337
yehudhamuman <shinryujin4646@gmail.com>
2026-07-11 19:25:34 +0700

    menghapus isi file kontrak.py

--- models/kontrak.py
+++
@@ -1,6 +0,0 @@
-class Kontrak:
-    def __init__(self, penghuni, kamar, tanggal_masuk, status_sewa):
-        self.penghuni = penghuni
-        self.kamar = kamar
-        self.tanggal_masuk = tanggal_masuk
-        self.status_sewa = status_sewa
